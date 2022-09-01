28f4f10b725b6e4a54961255c518d8b53d5e76d7
MRW Neundorf <ninevillage@gmail.com>
2022-09-01 12:13:46 +0200

    feat: init flutter app

---
+++ lib/screens/AboutScreen.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class AboutScreen extends StatelessWidget {
+  const AboutScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('About'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            // Navigate back to the home screen by popping the current route
+            // off the stack.
+            Navigator.pop(context);
+          },
+          child: const Text('Go back!'),
+        ),
+      ),
+    );
+  }
+}
